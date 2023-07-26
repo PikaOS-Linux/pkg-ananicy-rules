2b55428b04bfd3ea1aac842209ff0cb3945ec0f3
ferrreo <harderthanfire@gmail.com>
2023-07-26 16:18:38 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,6 +6,8 @@ DEBIAN_FRONTEND=noninteractive
 cd ananicy-rules
 git clone https://github.com/CachyOS/ananicy-rules.git ./etc/ananicy.d
 
+mv -f ./ananicy.conf ./etc/ananicy.d/ananicy.conf
+
 # Get build deps
 apt-get build-dep ./ -y
 
